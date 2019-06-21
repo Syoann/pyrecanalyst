9539d88dc0574408b85be9770840157ccb02714c
Yoann <yseeleuthner@gmail.com>
2019-06-21 11:43:40 +0200

    Fixed a bug in map name translation

--- src/Processors/MapName.py
+++ src/Processors/MapName.py
@@ -12,7 +12,7 @@ class MapName:
 
         # Possible formats for map type lines in the Objectives tab.
         # The Map Type strings used in the Objectives message tab are stored in
-        # string id 9654. self array was generated using:
+        # string id 9654. This array was generated using:
         # grep "9654" "SteamApps/common/Age2HD/resources/*/strings/key-value/key-value-strings-utf8.txt"
         #
         # HD Edition uses UTF-8, but older versions used localization-specific code pages.
@@ -30,6 +30,9 @@ class MapName:
             'nl': {'content': 'Kaarttype: (.*)', 'encoding': 'utf-8'},
             'ru': {'content': 'Тип карты: (.*)', 'encoding': 'windows-1251'},
             'zh': {'content': '地图类型: (.*)', 'encoding': 'cp936'},
+            'jp_utf-8': {'content': 'マップの種類: (.*)', 'encoding': 'utf-8'},
+            'ko_utf-8': {'content': '지도 종류: (.*)', 'encoding': 'utf-8'},
+            'zh_utf8': {'content': '地图类型: (.*)', 'encoding': 'utf-8'},
         }
 
     def run(self):
@@ -38,9 +41,7 @@ class MapName:
         messages = header.messages
         instructions = messages["instructions"]
 
-        lines = instructions.split(b'\n')
-
-        for line in lines:
+        for line in instructions.split(b'\n'):
             # We don't know what language the game was played in, so we try
             # every language we know.
             for lang, attr in self.map_type_regexes.items():
